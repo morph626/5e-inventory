af3d7e14499f4c4f0438a5ecb8753bacb2cc70ae
Marie-Louise Hedegaard <mallo1616@hotmail.com>
2018-07-07 21:18:57 +0200

    Add app bar

--- app/src/main/java/dk/venus626/dnd5einventory/MainActivity.kt
+++ app/src/main/java/dk/venus626/dnd5einventory/MainActivity.kt
@@ -2,11 +2,33 @@ package dk.venus626.dnd5einventory
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
+import android.view.Menu
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val spinner = findViewById(R.id.spinner_character) as Spinner
+
+        val adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.planets_array,
+                android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
+        spinner.adapter = adapter
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.actions_main, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
