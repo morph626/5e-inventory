5976fc19dd7c7b2b9d0b670433d8be330d8a6931
Marie-Louise Hedegaard <mallo1616@hotmail.com>
2018-07-08 21:45:08 +0200

    Add scale bar
    Signed-off-by: Marie-Louise Hedegaard <mallo1616@hotmail.com>

--- app/src/main/java/dk/venus626/dnd5einventory/CoinsFragment.kt
+++ app/src/main/java/dk/venus626/dnd5einventory/CoinsFragment.kt
@@ -6,8 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class PurseFragment : Fragment() {
+class CoinsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_purse, container, false)
+        return inflater.inflate(R.layout.fragment_coins, container, false)
     }
 }
